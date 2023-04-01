ff508781e30b956b1cb045fcfe97bec7c768ea98
Simon Gellrich <code.simgel@ge-mx.net>
2023-04-01 22:50:42 +0200

    job cancellation

--- cron-core/src/test/java/de/g3s1/oss/cron/test/works/SchedulerTests.java
+++ cron-core/src/test/java/de/g3s1/oss/cron/test/works/SchedulerTests.java
@@ -1,5 +1,6 @@
 package de.g3s1.oss.cron.test.works;
 
+import de.g3s1.oss.cron.api.CronEntry;
 import de.g3s1.oss.cron.works.CronTaskScheduler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,6 @@ import java.util.concurrent.Executors;
 public class SchedulerTests {
     @Test
     public void simpleTest() throws InterruptedException {
-
         CronTaskScheduler scheduler = new CronTaskScheduler(Executors.newSingleThreadExecutor());
         scheduler.startThread();
 
@@ -30,5 +30,24 @@ public class SchedulerTests {
         Assertions.assertEquals(1, dummyCronTask.getExecutions().size());
 
         System.out.println(LocalDateTime.ofInstant(dummyCronTask.getExecutions().get(0), ZoneOffset.UTC));
+    }
+
+    @Test
+    public void testCancellation() throws InterruptedException {
+        CronTaskScheduler scheduler = new CronTaskScheduler(Executors.newSingleThreadExecutor());
+        scheduler.startThread();
+
+        DummyCronTask dummyCronTask = new DummyCronTask();
+        DummyCronTrigger trigger = new DummyCronTrigger();
+
+        Instant t1 = Instant.now();
+        CronEntry entry = scheduler.submit(trigger, dummyCronTask);
+        dummyCronTask.waitForExecution(2000);
+        Instant t2 = Instant.now();
+
+        entry.cancel();
+
+        Assertions.assertTrue((t2.toEpochMilli() - t1.toEpochMilli()) < 2000);
+        Assertions.assertEquals(1, dummyCronTask.getExecutions().size());
     }
 }
