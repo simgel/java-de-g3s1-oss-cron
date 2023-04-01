3073ec8c7de7f1d6a9a7df29a69df0926d9d38ca
Simon Gellrich <code.simgel@ge-mx.net>
2023-04-01 21:28:41 +0200

    trigger test

--- cron-core/src/test/java/de/g3s1/oss/cron/test/works/trigger/AbstractTriggerTest.java
+++ cron-core/src/test/java/de/g3s1/oss/cron/test/works/trigger/AbstractTriggerTest.java
@@ -1,4 +1,12 @@
 package de.g3s1.oss.cron.test.works.trigger;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 public abstract class AbstractTriggerTest {
+    protected Instant getBaseTime() {
+        LocalDateTime localDateTime = LocalDateTime.parse("2023-04-01T12:00:00");
+        return localDateTime.toInstant(ZoneOffset.UTC);
+    }
 }
--- cron-core/src/test/java/de/g3s1/oss/cron/test/works/trigger/NextSecondTriggerTest.java
+++ cron-core/src/test/java/de/g3s1/oss/cron/test/works/trigger/NextSecondTriggerTest.java
@@ -1,10 +1,20 @@
 package de.g3s1.oss.cron.test.works.trigger;
 
+import de.g3s1.oss.cron.works.trigger.NextSecondCronTrigger;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
 
 public class NextSecondTriggerTest extends AbstractTriggerTest {
     @Test
     public void testTrigger() {
+        NextSecondCronTrigger trigger = new NextSecondCronTrigger();
 
+        Instant baseTime = getBaseTime();
+        Instant next = trigger.nextExecution(baseTime);
+
+        long diff = next.toEpochMilli() - baseTime.toEpochMilli();
+        Assertions.assertTrue(diff >= 1000);
     }
 }
