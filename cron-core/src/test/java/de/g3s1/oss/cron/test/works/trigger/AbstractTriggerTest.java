a763a34948a39afae4e0f750dcc099bf7fbf4b3e
Simon Gellrich <code.simgel@ge-mx.net>
2023-04-01 21:24:08 +0200

    setup junit

---
+++ cron-core/src/test/java/de/g3s1/oss/cron/test/works/trigger/AbstractTriggerTest.java
@@ -0,0 +1,4 @@
+package de.g3s1.oss.cron.test.works.trigger;
+
+public abstract class AbstractTriggerTest {
+}
---
+++ cron-core/src/test/java/de/g3s1/oss/cron/test/works/trigger/NextSecondTriggerTest.java
@@ -0,0 +1,10 @@
+package de.g3s1.oss.cron.test.works.trigger;
+
+import org.junit.jupiter.api.Test;
+
+public class NextSecondTriggerTest extends AbstractTriggerTest {
+    @Test
+    public void testTrigger() {
+
+    }
+}
